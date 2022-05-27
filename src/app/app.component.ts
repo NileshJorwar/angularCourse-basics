f07cec34241964dd76942db422465c16273f9488
NileshJorwar <jorwarnilesh@gmail.com>
2022-05-27 05:47:29 -0500

    adding mvc style for project"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `<p>Hello world!</p>`,
+  // template: `<p>Hello world!</p>`,
+  templateUrl: './app.component.html',
+  // styles:[],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent
 {
