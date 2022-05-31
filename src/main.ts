d7d04071fc465d98175bb1da88ab2154e7475586
NileshJorwar <jorwarnilesh@gmail.com>
2022-05-31 07:06:43 -0500

    expressions and interpolation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { enableProdMode } from "@angular/core";
-// change detection in app
-
+// change detection run once in app and should have env var set  and run before main app runs or will have it running change detection twice 
+import { environment } from "./environments/environment";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from './app/app.module'
+
+if(environment.production){
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule( AppModule )
 .catch(err => console.error(err));
