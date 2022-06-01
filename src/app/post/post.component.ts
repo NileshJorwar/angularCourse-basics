6b4fd1a6affe924e50106e9453817a740c3b7303
NileshJorwar <jorwarnilesh@gmail.com>
2022-06-01 07:23:11 -0500

    lifecycle hooks

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,12 +1,70 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component, Input, EventEmitter, Output, OnInit, OnChanges, DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit, AfterViewChecked
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent  {
+
+export class PostComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked {
 
   @Input('img') postImg = ''
   @Output() imageSelected = new EventEmitter<string>();
+
+  //first function to run
+  constructor() {
+    //get called during initialization of our component
+    console.log('constructor called', this.postImg)
+    //runs once 
+
+  }
+
+  //another hook when our comp is initialized with data
+  ngOnInit() {
+
+    console.log('ngOnInit called', this.postImg)
+    //runs after binding has occurred
+    //should be used when data is available from parent to child
+    //runs once 
+
+  }
+
+  ngOnChanges() {
+
+    console.log('ngOnChanges called', this.postImg)
+    //runs whenever changes made to the compo
+    //guaranteed to run once in our comp
+
+  }
+
+  ngDoCheck() {
+
+    console.log('ngDoCheck called', this.postImg)
+    //runs after change detection cycle has occurred
+    //for performing changes when angular dont want to
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called')
+      
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called')
+ 
+  }
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called')
+ 
+  } 
+  
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called')
+ 
+  }
 }
